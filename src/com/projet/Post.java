03f98f6c56ab3d088eb2629d452b7fcbdedaecd4
Mohamed Kaba <muhamad.kaba@gmail.com>
2018-12-09 22:00:12 +0100

    added constructors

--- src/com/projet/Post.java
+++ src/com/projet/Post.java
@@ -5,4 +5,11 @@ public class Post {
 	private int id;
 	private String title;
 	private String content;
+	
+	public Post(int id, String title, String content) {
+		this.id = id;
+		this.title = title;
+		this.content = content;
+
+	}
 }
