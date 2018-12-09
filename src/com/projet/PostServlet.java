06a31f2e96b2c9e54bcd825de903eb473aaad89a
Mohamed Kaba <muhamad.kaba@gmail.com>
2018-12-09 21:41:09 +0100

    forward request from servlet to jsp

--- src/com/projet/PostServlet.java
+++ src/com/projet/PostServlet.java
@@ -27,15 +27,7 @@ public class PostServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		this.getServletContext().getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
 	}
 
 }
