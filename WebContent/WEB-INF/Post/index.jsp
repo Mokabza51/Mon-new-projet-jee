1f23c7db1e6369b6f68b95729bafa82b8869b3e9
Mohamed Kaba <muhamad.kaba@gmail.com>
2018-12-10 11:02:13 +0100

    forwarded a list of post from servlet to jsp

--- WebContent/WEB-INF/Post/index.jsp
+++ WebContent/WEB-INF/Post/index.jsp
@@ -1,6 +1,7 @@
-<%@page import="posts.Post"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@page import="com.projet.posts.Post"%>
+<%@ page import="java.util.List" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,8 +11,9 @@
 <body>
 <h1>My posts</h1>
 <ul>
-	<% Post post = (Post) request.getAttribute("post"); %>
-	<li><a href="?id=<%= post.getId() %>"><%= post.getTitle() %></a></li>
+	<% for(Post post : (List<Post>)request.getAttribute("posts")) { %>
+		<li><a href="?id=<%= post.getId() %>"><%= post.getTitle() %></a></li>
+	<% } %>
 </ul>
 </body>
 </html>
--- src/com/projet/posts/PostServlet.java
+++ src/com/projet/posts/PostServlet.java
@@ -1,6 +1,8 @@
 package com.projet.posts;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,10 +29,17 @@ public class PostServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		var post = new Post(1, "lorem ipsum", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
-		request.setAttribute("post", post);
 		
-		this.getServletContext().getRequestDispatcher("/WEB-INF/posts/index.jsp").forward(request, response);
+		var posts = new ArrayList<Post>();
+		posts.add(
+			new Post(1, "lorem ipsum", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+		);
+		posts.add(
+			new Post(2, "elsass ipsum", "Lorem Elsass ipsum Wurschtsalad DNA, geïz commodo placerat.")
+		);
+		request.setAttribute("posts", posts);
+		
+		this.getServletContext().getRequestDispatcher("/WEB-INF/Post/index.jsp").forward(request, response);
 	}
 
 }
